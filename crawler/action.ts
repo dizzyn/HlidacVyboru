9c1aaf0aeb17b0bc08c19bcb543c2d6fb458b6eb
Tomas Randus <tom.randus@gmail.com>
2021-01-05 07:49:10 +0100

    vybory

--- crawler/action.ts
+++ crawler/action.ts
@@ -2,21 +2,21 @@ import Crawler from "crawler";
 import { BASE_URL } from "../pages";
 
 export default (uri: string): Object => {
-  // console.log("`${BASE_URL}${uri}`", `${BASE_URL}${uri}`);
+  console.log("`${BASE_URL}${uri}`", `${BASE_URL}${uri}`);
   return new Promise((resolve, reject) => {
     new Crawler({
       callback: function (error, { $ }, done) {
         if (error) {
           reject(error);
         } else {
-          const title = $("h1").text();
-          const items = $(".news-item.no-date .news-item-title a")
-            .map((_, a) => {
-              return { title: $(a).text(), href: $(a).attr("href") };
-            })
-            .toArray();
+          const title = $("#main-content b").text();
+          // const items = $(".news-item.no-date .news-item-title a")
+          //   .map((_, a) => {
+          //     return { title: $(a).text(), href: $(a).attr("href") };
+          //   })
+          //   .toArray();
 
-          resolve({ items, title });
+          resolve({ title });
         }
         done();
       },
--- pages/action/[href].tsx
+++ pages/action/[href].tsx
@@ -1,20 +1,23 @@
 
 import { GetServerSideProps } from "next";
 import Layout from "../../components/Layout";
-import vybor from "../../crawler/vybor";
+import action from "../../crawler/action";
 
-interface TAction {
+interface TActionDetail {
   title: string;
   href: string;
 }
 
-const IndexPage = ({ items, title }: { items: TAction[], title: string }) => (
+const IndexPage = ({ action: { title } }: { action: TActionDetail }) => (
   <Layout title={title}>
     <h1>{title}</h1>
+    Datum jednání	6. ledna 2021
+    Číslo jednání	47
+    Projednávané dokumenty:
     <ul>
-      {items.map((i) => (
-        <li><a href={`/action/${encodeURIComponent(i.href)}`}>{i.title}</a></li>
-      ))}
+      {/* {items.map((i) => (
+        <li><a href={`/vybor/${encodeURIComponent(i.href)}`}>{i.title}</a></li>
+      ))} */}
     </ul>
   </Layout>
 );
@@ -22,9 +25,8 @@ const IndexPage = ({ items, title }: { items: TAction[], title: string }) => (
 export const getServerSideProps: GetServerSideProps = async (context) => {
   // console.log("HREF", decodeURIComponent(String(context.params?.href)))
   const uri = decodeURIComponent(String(context.params?.href))
-  // console.log("a", await vybor(uri))
   return {
-    props: { ...(await vybor(uri)) },
+    props: { action: await action(uri) },
   };
 };
 
